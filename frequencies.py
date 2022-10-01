00e3bb2bf4efc56059e0fee9f19e54840ab3b4f7
Ashley Tyagi <ashleytyagi@outlook.com>
2022-10-01 05:46:18 +0100

    Update code to work

--- frequencies.py
+++ frequencies.py
@@ -1,11 +1,15 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def frequencies(*items):
+def frequencies(items):
     frequencies = {}
     for value in items:
         if str(value) in frequencies:
             frequencies[str(value)] = frequencies[str(value)] + 1
         else:
-            frequencies[str(value)] = 0
+            frequencies[str(value)] = 1
     return frequencies
+
+
+input = ['0', 4,4,'4','d','d','e',0,'a','d','4']
+frequencies(input)
